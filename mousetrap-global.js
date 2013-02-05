a7c3d96cee8b12707841fde167c89f0573366b24
Dave Irvine <dave@daveirvine.me>
2013-02-05 10:28:09 +0000

    Remove unnecessary function wrapper
    Mousetrap exposes itself to the global namespace, so "Mousetrap"
    already exists.
    
    The require call was pulling in Mousetrap as a module, but defining
    it locally as "mousetrap", which was never used.

--- mousetrap-global.js
+++ mousetrap-global.js
@@ -1,8 +1,6 @@
 require([
     'mousetrap'
-],
-
-function(mousetrap) {
+], function () {
 
     // https://gist.github.com/3885446
     // https://github.com/ccampbell/mousetrap/pull/64
@@ -14,31 +12,27 @@ function(mousetrap) {
      * usage:
      * Mousetrap.bindGlobal('ctrl+s', _saveChanges);
      */
-    Mousetrap = (function(Mousetrap) {
-        var _global_callbacks = {},
-            _original_stop_callback = Mousetrap.stopCallback,
-            _original_unbind = Mousetrap.unbind;
+    var _global_callbacks = {},
+        _original_stop_callback = Mousetrap.stopCallback,
+        _original_unbind = Mousetrap.unbind;
 
-        Mousetrap.stopCallback = function(e, element, combo) {
-            if (_global_callbacks[combo]) {
-                return false;
-            }
+    Mousetrap.stopCallback = function (e, element, combo) {
+        if (_global_callbacks[combo]) {
+            return false;
+        }
 
-            return _original_stop_callback(e, element, combo);
-        };
+        return _original_stop_callback(e, element, combo);
+    };
 
-        Mousetrap.bindGlobal = function(keys, callback, action) {
-            Mousetrap.bind(keys, callback, action);
-            _global_callbacks[keys] = true;
-        };
+    Mousetrap.bindGlobal = function (keys, callback, action) {
+        Mousetrap.bind(keys, callback, action);
+        _global_callbacks[keys] = true;
+    };
 
-        Mousetrap.unbind = function(keys, action) {
-            delete _global_callbacks[keys];
-            return _original_unbind.apply(Mousetrap, [keys, action]);
-        };
-
-        return Mousetrap;
-    })(Mousetrap);
+    Mousetrap.unbind = function (keys, action) {
+        delete _global_callbacks[keys];
+        return _original_unbind.apply(Mousetrap, [keys, action]);
+    };
 
     return Mousetrap;
 });
