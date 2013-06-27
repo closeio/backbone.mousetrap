544fc50ad95834c57bc608887ecaa1103090ea06
Phil Freo <phil@philfreo.com>
2013-06-27 15:45:09 -0700

    Use bindGlobal (if plugin exists), when using new 'mod' key

--- backbone.mousetrap.js
+++ backbone.mousetrap.js
@@ -1,4 +1,5 @@
 (function(_, Backbone, Mousetrap) {
+    'use strict';
 
     var oldDelegateEvents = Backbone.View.prototype.delegateEvents;
     var oldUndelegateEvents = Backbone.View.prototype.undelegateEvents;
@@ -15,7 +16,10 @@
                 if (!_.isFunction(method)) method = this[events[key]];
                 if (!method) throw new Error('Method "' + events[key] + '" does not exist');
                 method = _.bind(method, this);
-                if ('bindGlobal' in Mousetrap && (key.indexOf('command') !== -1 || key.indexOf('ctrl') !== -1)) {
+
+                // Use global-bind plugin when appropriate
+                // https://github.com/ccampbell/mousetrap/tree/master/plugins/global-bind
+                if ('bindGlobal' in Mousetrap && (key.indexOf('mod') !== -1 || key.indexOf('command') !== -1 || key.indexOf('ctrl') !== -1)) {
                     Mousetrap.bindGlobal(key, method);
                 } else {
                     Mousetrap.bind(key, method);
